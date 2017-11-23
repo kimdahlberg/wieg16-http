552854783b69e218dbe7231d3ddc2a6eb65619ea
kimdahlberg <dahlberg.kim@hotmail.com>
2017-11-23 14:02:09 +0100

    secund commit

--- functions.php
+++ functions.php
@@ -1,8 +1,42 @@
 <?php
-// "HTTP/1.0 404 Not Found"
-function status_header($code = 200) {}
+//ÖVNING 1
+
+//"HTTP/1.0 404 Not Found"
+function status_header($code = 200) {
+    $messages = [
+        200 => "OK",
+        500 => "Server Error"
+    ];
+    header("HTTP/1.0 $code " .$messages[$code]);
+}
+status_header(200);
+
+
+//ÖVNING 2
 
 // [header => värde]
-function headers(array $headers = []) {}
+// Connection: keep-alive
+function headers(array $headers = []) {
+    foreach ($headers as $header => $value){
+        header("$header: $value");
+    }
+}
 
-function redirect($url, $code = 302) {}
+headers([
+    "Connection" => "Upgrade"
+    "johan"      => "victor"
+]);
+
+
+//ÖVNING 3
+
+function redirect($url, $code = 302) {
+    header('Location: ' . $url, true, $code);
+    die();
+}
+
+// $code = 302
+//redirect("https://google.se");
+
+// $code = 301
+//redirect("https://google.se", 301);
